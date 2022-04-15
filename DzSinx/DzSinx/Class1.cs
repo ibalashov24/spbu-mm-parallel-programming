52d782a472c60589fdc453e4d481880717926a56
TheGadji <96838917+TheGadji@users.noreply.github.com>
2022-04-15 19:16:57 +0300

    Add files via upload

---
+++ DzSinx/DzSinx/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzSinx
+{
+
+    public class Class1
+    {
+        private readonly Mutex mutex = new Mutex();
+        private readonly Queue<int> queue = new Queue<int>();
+
+        public void Send(int i)
+        {
+            mutex.WaitOne();
+            try
+            {
+                queue.Enqueue(i);
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+
+        public int Recieve()
+        {
+            while (true)
+            {
+                mutex.WaitOne();
+                try
+                {
+                    if (queue.TryDequeue(out var result))
+                    {
+                        return result;
+                    }
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ DzSinx/DzSinx/Consumer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzSinx
+{
+
+    public class Consumer
+    {
+        private readonly Mutex mutex = new Mutex();
+        private Thread? thread;
+        private readonly int number;
+        private volatile bool activated = false;
+
+        public Consumer(int number)
+        {
+            this.number = number;
+        }
+        public void Stop()
+        {
+            activated = false;
+        }
+
+        public bool Run(Class1 clas)
+        {
+            mutex.WaitOne();
+            try
+            {
+                if (activated)
+                {
+                    return false;
+                }
+
+                thread = new Thread(() =>
+                {
+                    while (activated)
+                    {                        
+                        Console.WriteLine($"C {number} - P {clas.Recieve()}");
+                        Thread.Sleep(3000);
+                    }
+                });
+                thread.Name = $"consumer_{number}";
+                activated = true;
+                thread.Start();
+                return activated;
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }        
+    }
+}
